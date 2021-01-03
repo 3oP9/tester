04b0d6b4aaa35ec84d3e48b2d8d666a4a454ff8e
o9P2 <76880459+3oP9@users.noreply.github.com>
2021-01-03 03:09:44 +0300

    Create pest.py

---
+++ pest.py
@@ -0,0 +1,13 @@
+import os
+os.system("clear")
+import androidhelper
+droid = androidhelper.Android()
+droid.makeToast('منوريين سكربت الشبح..!')
+
+def v7x (name) :
+    name = input ("type your name: ")
+    if name == 'haedr' :
+        print('welcome', name)
+    else:
+        print (" Error" *100)
+v7x ("ha")
